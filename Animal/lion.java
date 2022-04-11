d76a605b92eb6edb154262d1dbffb3a7d475a4c4
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-04-12 01:16:50 +0500

    Update

--- Animal/lion.java
+++ Animal/lion.java
@@ -5,23 +5,28 @@ class lion extends animal{
     lion(){ //Null Construtor
     }
 
-    lion(n a){ //parameterized constructor
+    lion(String name, String n, int a){ //parameterized constructor
+        super (name, n , a);
         this.n = name;
         this.a = age;
     }
 
     lion (lion other){ //copy constructor
+        super(other);
         name = other.name;
         age = other.age;
     }
 
     void display(){ //display function
+        super.display();
         System.out.println("Name :" + name);
         System.out.println("Age :" + age);
     }
+
+    
 }
 
 public static void main(String[] args) {
     lion amas = new lion();
-    lion.display("am" 16);
+    
 }
