f1afb488e07ffd33014a37d7a91b036429577b73
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-04-17 02:59:12 +0500

    Updated

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+#include thing.h
+using namespace std;
 int main(){
+    thing T(1);
+    cout<<endl;
+
+    do2(2);
+    cout<<endl;
+
+    do1(3);
+    cout<<endl;
+
+    do2(5);
+    cout<<endl;
+    
     return 0;
-}
+};
--- cpp/thing.cpp
+++ cpp/thing.cpp
@@ -20,4 +20,14 @@ class thing {
     thing :: ~thing(){
         cout<<"Destructing thing..."<<*x<<endl;
     }
+
+    void do2(int value){
+        cout<<"Enter do2 with level="<<level<<endl;
+    }
+
+    void do1(int value){
+        cout<<"Enter do1 with level="<<level<<endl;
+        thing TTT(level);
+        do2(level+1);
+    }
 }
